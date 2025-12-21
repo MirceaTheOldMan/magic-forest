9388092d8a4bb2e480348191d7435f36dab4712d
Mircea Vlad <vlad.mircea@gmail.com>
2025-12-21 15:14:48 +0200

    Modificari decembrie 2025

--- src/app/shared/services/price.service.ts
+++ src/app/shared/services/price.service.ts
@@ -11,45 +11,38 @@ export class PriceService {
 
 const prices = [
   {
-    week: 1199,
-    weekend: 1499,
+    week: 1200,
+    weekend: 1500,
     copii: '10',
     isMax: false,
     adulti: '20',
   },
   {
-    week: 1599,
-    weekend: 1799,
+    week: 1600,
+    weekend: 1800,
     copii: '10',
     isMax: true,
     adulti: '20',
   },
   {
-    week: 1399,
-    weekend: 1699,
+    week: 1400,
+    weekend: 1700,
     copii: '15',
     isMax: true,
     adulti: '25',
   },
   {
-    week: 2199,
-    weekend: 2599,
+    week: 2200,
+    weekend: 2600,
     copii: '20',
     isMax: true,
     adulti: '30',
   },
   {
-    week: 2499,
-    weekend: 2899,
+    week: 2500,
+    weekend: 2900,
     copii: '25',
     isMax: true,
     adulti: '40',
-  },
-  {
-    week: 3199,
-    weekend: 3899,
-    copii: '30',
-    isMax: true,
-    adulti: '40',
-  },
+  }
 ];
